f27ddec6060065fd100639f91aebeb9733d4d6f5
Alexander Pinske <alex@pinske.eu>
2022-01-19 19:45:06 +0100

    spring init

---
+++ src/main/java/eu/pinske/playgroundhttp/PlaygroundHttpApplication.java
@@ -0,0 +1,13 @@
+package eu.pinske.playgroundhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/pinske/playgroundhttp/PlaygroundHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.pinske.playgroundhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
